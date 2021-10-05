404e8b1577be42312a0af9f00e25e478c821cc10
94rain <94rain@msn.com>
2021-10-05 18:48:29 -0500

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -23,7 +23,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/94rain/portfolio">
-            Built upon Next.js & Tailwind CSS
+            Built upon Next.js & Tailwind CSS, deployed with Github Actions
           </Link>
         </div>
       </div>
